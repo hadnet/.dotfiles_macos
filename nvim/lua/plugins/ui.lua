db909f6592206505df9dc1ac42653a3b552248bf
hadnet <hadnet1@gmail.com>
2025-02-24 03:24:03 -0300

    make bufferline to distinguish index.tsx | page.tsx files

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -69,22 +69,35 @@ return {
   },
 
   -- buffer line
-  -- {
-  --   "akinsho/bufferline.nvim",
-  --   event = "VeryLazy",
-  --   keys = {
-  --     { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-  --     { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  --   },
-  --   opts = {
-  --     options = {
-  --       mode = "tabs",
-  --       -- separator_style = "slant",
-  --       show_buffer_close_icons = false,
-  --       show_close_icon = false,
-  --     },
-  --   },
-  -- },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        -- mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        name_formatter = function(buf)
+          local filename = vim.fn.fnamemodify(buf.name, ":t")
+          if filename == "index.tsx" or filename == "page.tsx" then
+            local parent = vim.fn.fnamemodify(buf.name, ":h:t")
+            if parent == "." then
+              return filename
+            else
+              return parent .. "/" .. filename
+            end
+          else
+            return filename
+          end
+        end,
+      },
+    },
+  },
   {
     "mg979/vim-visual-multi",
     branch = "master",
